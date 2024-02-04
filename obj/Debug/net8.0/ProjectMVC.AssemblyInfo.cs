7a61a933a02a49d52f10a8dbff388ee0630b4f5a
victorhtanaka <victorhtanaka555@gmail.com>
2024-02-04 10:21:07 -0300

    carro

--- obj/Debug/net8.0/ProjectMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1336764332276fe58830b2afe6368d9776da589a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac44de40c2626fc9406f4be890aa7b2dddce546")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
